0f8a856831962d0597bd7f602f8b8ea7d17f94db
A-lineed <aline.edvania.franca@outlook.com>
2024-03-19 11:17:50 -0300

    Estrutura de cenário usando função

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,10 +1,24 @@
-const {Builder, Browser} = require('selenium-webdriver');
+const { Builder, Browser, By, Key } = require("selenium-webdriver");
+const assert = require("assert");
 
 async function example() {
 
-//Instanciando navegador 
-let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    //Instanciando navegador 
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
+    //Navegando pela aplicação 
+    await driver.get("https://lambdatest.github.io/sample-todo-app/")
 
+    //Primeiro teste - Preenchendo campo 
+    await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+    //assert 
+    let todoText = await driver.findElement(By.xpath("/html/body/div/div/h2")).getText()
+
+    //assert using node assertion 
+    assert.strictEqual(todoText, "LambdaTest Sample App");
+
+    //Fechando o navegador
+    await driver.quit()
 }
 example()
